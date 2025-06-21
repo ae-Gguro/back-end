6691c43a340985665a73850132aa18a42dcca426
hmj6589 <hmj6589@gmail.com>
2025-06-21 15:13:00 +0900

    🎉 Init : 프로젝트 초기 세팅

---
+++ src/main/java/com/example/gguro/GguroApplication.java
@@ -0,0 +1,13 @@
+package com.example.gguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GguroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GguroApplication.class, args);
+    }
+
+}
