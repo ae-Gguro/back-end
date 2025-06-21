59cb3e041065e57859a9c194709378e412e41840
hmj6589 <hmj6589@gmail.com>
2025-06-21 16:31:39 +0900

    ✨ Feat : User, Profile 엔티티 구현

---
+++ src/main/java/com/example/gguro/domain/Profile.java
@@ -0,0 +1,21 @@
+package com.example.gguro.domain;
+
+import com.example.gguro.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Profile extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
