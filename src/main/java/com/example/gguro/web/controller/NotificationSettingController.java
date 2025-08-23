4fa1d2103d407d2023bd351b04614b46c0b0be48
hmj6589 <hmj6589@gmail.com>
2025-08-23 17:14:12 +0900

    Feat: 알림 세팅 설정 및 조회 controller 작성

---
+++ src/main/java/com/example/gguro/web/controller/NotificationSettingController.java
@@ -0,0 +1,48 @@
+package com.example.gguro.web.controller;
+
+import com.example.gguro.apiPayload.ApiResponse;
+import com.example.gguro.domain.User;
+import com.example.gguro.service.NotificationSettingService.NotificationSettingCommandService;
+import com.example.gguro.service.NotificationSettingService.NotificationSettingQueryService;
+import com.example.gguro.web.dto.notificationSetting.NotificationSettingRequestDTO;
+import com.example.gguro.web.dto.notificationSetting.NotificationSettingResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.gguro.jwt.FindLoginUser.getCurrentUser;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "알림 세팅 API", description = "알림 세팅 관련 API입니다.")
+public class NotificationSettingController {
+
+    private final NotificationSettingCommandService notificationSettingCommandService;
+    private final NotificationSettingQueryService notificationSettingQueryService;
+
+    @PatchMapping("/api/notification_setting/{profileId}")
+    @Operation(summary = "해당 프로필의 알림 세팅 변경", description = "해당 프로필의 알림 세팅을 변경할 수 있는 페이지입니다.")
+    public ApiResponse<String> updateNotificationSetting(
+            @PathVariable("profileId") Long profileId,
+            @RequestBody NotificationSettingRequestDTO request
+    ) {
+        User user = getCurrentUser();
+        notificationSettingCommandService.upateNotificationSetting(user, profileId, request);
+        return ApiResponse.onSuccess("성공적으로 알림 세팅이 변경되었습니다.");
+    }
+
+    @GetMapping("/api/notification_setting/{profileId}")
+    @Operation(summary = "해당 프로필의 알림 세팅 조회", description = "해당 프로필의 알림 세팅을 조회할 수 있는 페이지입니다.")
+    public ApiResponse<NotificationSettingResponseDTO> getNotificationSetting(
+            @PathVariable("profileId") Long profileId
+    ) {
+        User user = getCurrentUser();
+        return ApiResponse.onSuccess(
+                notificationSettingQueryService.getNotificationSetting(user, profileId)
+        );
+    }
+
+}
