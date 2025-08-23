ba4f219fee9c7ca94c4be6a14e02dadf042516a2
hmj6589 <hmj6589@gmail.com>
2025-08-24 01:06:01 +0900

    Feat: 하루, 지난 주 리포트 푸시 알림 구현

--- src/main/java/com/example/gguro/repository/UserRepository.java
+++ src/main/java/com/example/gguro/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.example.gguro.repository;
 
 import com.example.gguro.domain.User;
-import com.example.gguro.domain.enums.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,4 +14,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     Optional<User> findByOauthId(String oauthId);
+
+    @Query("select distinct u from User u " +
+            "left join fetch u.profileList p " +
+            "left join fetch p.notificationSetting ns")
+    List<User> findAllWithProfilesAndSettings();
 }
