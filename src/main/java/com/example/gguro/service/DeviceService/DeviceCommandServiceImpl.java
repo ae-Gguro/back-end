96a5bcf2cd5f42e812fb814f86b71d00de38b25f
hmj6589 <hmj6589@gmail.com>
2025-08-26 03:22:59 +0900

    ✨ Feat : 로그아웃 시, 디바이스 토큰 비활성화 로직 작성

--- src/main/java/com/example/gguro/service/DeviceService/DeviceCommandServiceImpl.java
+++ src/main/java/com/example/gguro/service/DeviceService/DeviceCommandServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.gguro.service.DeviceService;
 
+import com.example.gguro.apiPayload.code.status.ErrorStatus;
+import com.example.gguro.apiPayload.exception.handler.UserHandler;
 import com.example.gguro.converter.DeviceConverter;
 import com.example.gguro.domain.Device;
 import com.example.gguro.domain.User;
 import com.example.gguro.repository.DeviceRepository;
+import com.example.gguro.repository.UserRepository;
 import com.example.gguro.web.dto.device.DeviceRequestDTO;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class DeviceCommandServiceImpl implements DeviceCommandService {
 
     private final DeviceRepository deviceRepository;
+    private final UserRepository userRepository;
 
     @Override
     // 사용자의 디바이스 토큰을 등록하거나 재활성화하는 기능(로그아웃했다가 로그인하는 과정을 고려)
@@ -30,6 +34,16 @@ public class DeviceCommandServiceImpl implements DeviceCommandService {
                     Device device = DeviceConverter.toDevice(user, request.getToken(), request.getDeviceType());
                     return deviceRepository.save(device);
                 });
+    }
+
+    @Override
+    // 디바이스 토큰 비활성화 (로그아웃 경우)
+    public void deactivateDeviceToken(Long userId, String token) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserHandler(ErrorStatus.USER_NOT_FOUND));
+
+        deviceRepository.findByUserAndToken(user, token)
+                .ifPresent(device -> device.updateActive(false));
     }
 
 }
