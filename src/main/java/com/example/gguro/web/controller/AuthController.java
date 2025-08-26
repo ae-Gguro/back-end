4830ccef8fc7119a7d93761324e4bfbaaf4015b9
황민지 <139426988+hmj6589@users.noreply.github.com>
2025-08-27 00:03:21 +0900

    ♻️ Refactor : import문 추가

--- src/main/java/com/example/gguro/web/controller/AuthController.java
+++ src/main/java/com/example/gguro/web/controller/AuthController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
 
 import static com.example.gguro.jwt.FindLoginUser.getCurrentUser;
 
