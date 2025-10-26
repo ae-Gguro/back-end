28ba735b268232f935a7c68b7b279127a68e6ba3
hmj6589 <hmj6589@gmail.com>
2025-10-26 11:23:31 +0900

    ♻️ Refactor: Firebase base64 디코딩 추가

--- src/main/java/com/example/gguro/config/FirebaseConfig.java
+++ src/main/java/com/example/gguro/config/FirebaseConfig.java
@@ -11,19 +11,22 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        String firebaseKey = System.getenv("FIREBASE_KEY_JSON");
-        if (firebaseKey == null || firebaseKey.isBlank()) {
+        String firebaseKeyBase64 = System.getenv("FIREBASE_KEY_JSON");
+
+        if (firebaseKeyBase64 == null || firebaseKeyBase64.isBlank()) {
             throw new IllegalArgumentException("FIREBASE_KEY_JSON 환경변수가 비어 있습니다!");
         }
 
-        try (InputStream serviceAccount =
-                     new ByteArrayInputStream(firebaseKey.getBytes(StandardCharsets.UTF_8))) {
+        // base64 디코딩 추가
+        byte[] decodedBytes = Base64.getDecoder().decode(firebaseKeyBase64);
+        try (InputStream serviceAccount = new ByteArrayInputStream(decodedBytes)) {
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -31,6 +34,7 @@ public class FirebaseConfig {
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
+                System.out.println("Firebase initialized successfully!");
             }
         }
     }
